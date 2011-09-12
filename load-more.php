b55414c350fb5e64ecb172267a08a0074f984e89
adam <adam@83cbd0d4-6396-413c-9aa4-74258c2feeb2>
2011-09-12 20:28:44 +0000

    Updated class and optimized

--- load-more.php
+++ load-more.php
@@ -2,43 +2,35 @@
 /*
 Plugin Name: HypLoadMore
 Plugin URI: http://hyperspatial.com
-Description: An ajax content loader system
+Description: An ajax content loading system for adding content dynamically
 Version: 1.0
 Author: Adam J Nowak
 Author URI: http://hyperspatial.com
 License: GPL2
 */
 
+/* HypLoadMore Class 
+*  Instantiate in the static php file */
 class HypLoadMore{
 	protected static $constructed;
-	protected $autoload,$class,$element_id,$offset,$limit,$path,$query,$query_array,$commands,$commands_array;
+	protected $autoload,$class,$element_id,$offset,$limit,$link,$path,$query,$query_array,$commands,$commands_array;
 	public function __construct($args = ''){
-		$defaults = array(
-			'autoload' => false,
-			'class' => '',
-			'element_id' => false,
-			'offset' => 0,
-			'limit' => 10,
-			'link' => 'Load More',
-			'path' => false,
-			'query' => false
-		);
-		//Store Vars
+		$defaults = array('autoload' => false,'class' => '','element_id' => false,'offset' => 0,'limit' => 10,'link' => 'Load More','path' => false,'query' => false);
+		//Store properties
 		$commands_array = $this->parse_args($args,$defaults);
 		extract($commands_array);
 		foreach($commands_array as $key => $value){ $this->{$key} = $value; }
-		//Commands
+		//Store reference arrays
 		$this->commands_array = $commands_array;
-		$this->commands = http_build_query($commands_array);
-		//Query Array
+		$this->commands = '?' . http_build_query($commands_array);
 		parse_str($this->query,$query_array);
 		$this->query_array = $query_array;
-		//Print and Load
+		//Print JS and load
 		if(!self::$constructed) $this->print_javascript();
 		if($autoload) $this->load();
 		self::$constructed ++;
 	}
-	/* ~~~~~~~~~~~~~~~~~~~ Private Methods ~~~~~~~~~~~~~~~~~~~ */
+	/* ~~~~~~~~~~~~~~~~~~~ Class Methods ~~~~~~~~~~~~~~~~~~~ */
 	
 	protected function parse_args($args,$defaults = ''){
     	if(is_object($args)) $r = get_object_vars($args);
@@ -47,34 +39,16 @@ class HypLoadMore{
         if(is_array($defaults)) return array_merge($defaults,$r);
         return $r;
     }
-	protected function parse_string($string, &$array) {
+	protected function parse_string($string, &$array){
     	parse_str($string,$array);
 		if(get_magic_quotes_gpc()) $array = stripslashes_deep($array);
      	return $array;
   	}
 	private function print_javascript(){?>
-        <script type="text/javascript">
-		var currentUrl;
-		function hlmLoad(target,url){
-			if(currentUrl == url){
-				document.getElementById(target).innerHTML = '';
-				currentUrl = '';
-				return;
-			}
-			else currentUrl = url;
-			if(window.XMLHttpRequest) xmlhttp = new XMLHttpRequest();
-			else xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-			xmlhttp.onreadystatechange = function(){ 
-				if(xmlhttp.readyState == 4 && xmlhttp.status == 200) document.getElementById(target).innerHTML += xmlhttp.responseText;
-			}
-			xmlhttp.open("GET",url,true);
-			xmlhttp.send();
-		}
-		</script>
+        <script type="text/javascript">var currentUrl;function hlmLoad(target,url){if(currentUrl==url){document.getElementById(target).innerHTML='';currentUrl='';return}else currentUrl=url;if(window.XMLHttpRequest)xmlhttp=new XMLHttpRequest();else xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");xmlhttp.onreadystatechange=function(){if(xmlhttp.readyState==4&&xmlhttp.status==200)document.getElementById(target).innerHTML+=xmlhttp.responseText};xmlhttp.open("GET",url,true);xmlhttp.send()}</script>
 		<?php
 	}	
-	
-	/* ~~~~~~~~~~~~~~~~~~~ Public Methods ~~~~~~~~~~~~~~~~~~~ */
+	/* ~~~~~~~~~~~~~~~~~~~ Accessors ~~~~~~~~~~~~~~~~~~~ */
 	
 	public function get_autoload(){ return $this->autoload; }
 	public function get_commands(){ return $this->commands; }
@@ -86,52 +60,43 @@ class HypLoadMore{
 	public function get_path(){ return $this->path; }
 	public function get_query(){ return $this->query; }
 	public function get_query_array(){ return $this->query_array; }
-	//Loader
-	public function load(){
-		$commands = $this->commands;
-		if($commands) $commands = '?' . $commands; 
-		?>
-		<script type="text/javascript">hlmLoad('<?php echo $this->element_id ?>','<?php echo $this->path . $commands ?>');</script>
+	
+	/* ~~~~~~~~~~~~~~~~~~~ Loader Methods ~~~~~~~~~~~~~~~~~~~ */
+	
+	public function load(){?>
+		<script type="text/javascript">hlmLoad('<?php echo $this->element_id ?>','<?php echo $this->path . $this->commands ?>');</script>
+		<?php
+	}
+	public function load_more(){?>
+		<span class="hlm-load-more <?php echo $this->class ?>" onclick="hlmLoad('<?php echo $this->element_id ?>','<?php echo $this->path . $this->commands ?>'); this.parentNode.removeChild(this);"><?php echo $this->link ?></span>
 		<?php
 	}
 }
 
-
+/* HypLoadMoreDynamic Class
+*  Instantiate in the dynamically loaded php file */
 class HypLoadMoreDynamic extends HypLoadMore{
 	public function __construct(){
-		$defaults = array(
-			'class' => '',
-			'element_id' => false,
-			'offset' => 0,
-			'limit' => 10,
-			'link' => 'Load More',
-			'path' => false,
-			'query' => false
-		);
+		$defaults = array('class' => '','element_id' => false,'offset' => 0,'limit' => 10,'link' => 'Load More','path' => false,'query' => false);
 		unset($_GET['autoload']);
-		//Store Vars
+		//Store properties
 		$commands_array = $this->parse_args($_GET,$defaults);
 		extract($commands_array);
 		foreach($commands_array as $key => $value){ $this->{$key} = $value; }
-		//Commands
+		//Store reference arrays
 		$this->commands_array = $commands_array;
-		$this->commands = http_build_query($commands_array);
-		//Query Array
+		$this->commands = '?' . http_build_query($commands_array);
 		parse_str($this->query,$query_array);
 		$this->query_array = $query_array;
 		self::$constructed ++;
 	}
-	/* ~~~~~~~~~~~~~~~~~~~ Public Methods ~~~~~~~~~~~~~~~~~~~ */
+	/* ~~~~~~~~~~~~~~~~~~~ Load More Method Overwrite ~~~~~~~~~~~~~~~~~~~ */
 	
 	public function load_more($results){
-		if(!$results) return;
-		if(count($results) < $this->limit) return;
-		$this->commands_array['offset'] = $this->commands_array['offset'] + $this->limit;
-		$this->commands = http_build_query($this->commands_array);
-		$commands = $this->commands;
-		if($commands) $commands = '?' . $commands; ?>
-        
-		<span class="hlm-load-more <?php echo $this->class ?>" onclick="hlmLoad('<?php echo $this->element_id ?>','<?php echo $this->path . $commands ?>'); this.parentNode.removeChild(this);"><?php echo $this->link ?></span>
+		if(!$results || count($results) < $this->limit) return;
+		$this->commands_array['offset'] = $this->commands_array['offset'] + $this->limit; //Shift offset for next load
+		$this->commands = '?' . http_build_query($this->commands_array); ?>
+		<span class="hlm-load-more <?php echo $this->class ?>" onclick="hlmLoad('<?php echo $this->element_id ?>','<?php echo $this->path . $this->commands ?>'); this.parentNode.removeChild(this);"><?php echo $this->link ?></span>
 		<?php
 	}
 }
